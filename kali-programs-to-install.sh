91d1a6a4ddca54fd8ebeb1f335d7600d6e423108
robertstrom <robert.strom@gmail.com>
2022-01-11 18:57:39 -0700

    Update and rename kali-programs-to-install to kali-programs-to-install.sh

---
+++ kali-programs-to-install.sh
@@ -0,0 +1,12 @@
+# Kali Setup programs to be installed and configuration changes needed
+
+## This collection of information is designed to make it easier to get a Kali instance to a standardized desired base configuration point
+## so that it is fully functional with all expected software installed.
+
+## pimpmykali - https://github.com/Dewalt-arch/pimpmykali
+rm -rf pimpmykali/
+cd pimpmykali
+sudo ./pimpmykali.sh
+
+## Install exa
+sudo apt install exa
