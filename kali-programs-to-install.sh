b900dd13c99f4baa3416ef14b2511426a0c66e21
robertstrom <robert.strom@gmail.com>
2022-01-11 20:17:57 -0700

    Update kali-programs-to-install.sh

--- kali-programs-to-install.sh
+++ kali-programs-to-install.sh
@@ -4,9 +4,18 @@
 ## so that it is fully functional with all expected software installed.
 
 ## pimpmykali - https://github.com/Dewalt-arch/pimpmykali
-rm -rf pimpmykali/
-cd pimpmykali
+git clone https://github.com/Dewalt-arch/pimpmykali /opt/pimpmykali
+rm -rf /opt/pimpmykali/
+cd /opt/pimpmykali
 sudo ./pimpmykali.sh
+cd  /
 
 ## Install exa
 sudo apt install exa
+
+## Create directory for storing downloaded exploits, etc.
+
+cd ~
+mkdir exploits
+## Clone Dewalt's pimpmy-phprevshell
+git clone https://github.com/Dewalt-arch/pimpmy-phprevshell.git
