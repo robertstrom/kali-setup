77db953c700a6e93fa1ec5ec7a2940d1f6945ddd
robertstrom <robert.strom@gmail.com>
2022-05-16 21:06:44 -0700

    Update kali-programs-to-install.sh

--- kali-programs-to-install.sh
+++ kali-programs-to-install.sh
@@ -131,6 +131,15 @@ sudo apt install kompare -y
 sudo apt install xxdiff -y
 sudo apt install krename -y
 
+# Install Visual Studio Code
+sudo apt update
+sudo apt install curl gpg gnupg2 software-properties-common apt-transport-https 
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update
+sudo apt install code
+
 # Install rustscan
 # https://overide.medium.com/rustscan-fcbdb93e17c9
 # https://github.com/RustScan/RustScan/wiki/Installation-Guide
