5dee18a66cdfae642bb5808fc91d0857621ed3be
robertstrom <robert.strom@gmail.com>
2022-05-16 21:35:15 -0700

    Update kali-programs-to-install.sh

--- kali-programs-to-install.sh
+++ kali-programs-to-install.sh
@@ -141,7 +141,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt update
-sudo apt install code
+sudo apt install code -y
 
 # Install rustscan
 # https://overide.medium.com/rustscan-fcbdb93e17c9
