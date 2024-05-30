9fdd743c9ebd5b017024f5623ac2c70f1cc38cab
robertstrom <robert.strom@gmail.com>
2024-05-29 19:27:56 -0700

    Update kali-programs-to-install.sh
    Removed geany and ncftp from the lilst of programs to install. They no longer appear to be offered by Kali repos

--- kali-programs-to-install.sh
+++ kali-programs-to-install.sh
@@ -204,9 +204,9 @@ wget https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/mas
 ## Install progress viewer
 # Install Geany IDE / Editor
 
-sudo apt install -yy shellcheck libimage-exiftool-perl pv geany terminator copyq xclip dolphin krusader kdiff3 krename kompare xxdiff krename kde-spectacle \
+sudo apt install -yy shellcheck libimage-exiftool-perl pv terminator copyq xclip dolphin krusader kdiff3 krename kompare xxdiff krename kde-spectacle \
 flameshot html2text csvkit remmina kali-wallpapers-all hollywood-activate kali-screensaver gridsite-clients shellter sipcalc \
-xsltproc rinetd torbrowser-launcher httptunnel kerberoast tesseract-ocr ncdu wkhtmltopdf grepcidr speedtest-cli neofetch sshuttle mpack filezilla ncftp lolcat \
+xsltproc rinetd torbrowser-launcher httptunnel kerberoast tesseract-ocr ncdu wkhtmltopdf grepcidr speedtest-cli neofetch sshuttle mpack filezilla lolcat \
 ripgrep bat dcfldd shellter redis-tools feroxbuster name-that-hash jq keepassxc okular exfat-fuse exfatprogs kate xsel
 
 # i3 program installs
